2d24b4e3266b6f6ddd91d3d1a713bc934944e942
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-19 05:52:07 +0700

    Create like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Like from "./common/like";
 import {getMovies} from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -8,6 +9,14 @@ class Movies extends Component {
 
     handleDelete = (movie) => {
         const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    };
+
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]};
+        movies[index].liked = !movies[index].liked;
         this.setState({movies});
     };
 
@@ -37,7 +46,14 @@ class Movies extends Component {
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
                             <td>
-                                <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
+                                <Like
+                                    liked={movie.liked}
+                                    onClick={() => this.handleLike(movie)}
+                                />
+                            </td>
+                            <td>
+                                <button
+                                    onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
                                     Delete
                                 </button>
                             </td>
