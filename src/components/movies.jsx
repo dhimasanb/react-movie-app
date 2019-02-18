cb5046a274b6399a650c0d928690a8db14a098e5
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-19 05:11:25 +0700

    Delete movie

---
+++ src/components/movies.jsx
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import {getMovies} from "../services/fakeMovieService";
+
+class Movies extends Component {
+    state = {
+        movies: getMovies()
+    };
+
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Movies Component</h2>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+}
+
+export default Movies;
