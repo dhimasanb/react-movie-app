32883073778d3ac691f594882d818a412488e089
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-20 06:06:25 +0700

    Handling selection

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -36,7 +36,7 @@ class Movies extends Component {
     };
 
     handleGenreSelect = (genre) => {
-        console.log(genre);
+        this.setState({ selectedGenre: genre});
     };
 
     render() {
@@ -53,6 +53,7 @@ class Movies extends Component {
                 <div className="col-3">
                     <ListGroup
                         items={this.state.genres}
+                        selectedItem={this.state.selectedGenre}
                         onItemSelect={this.handleGenreSelect}
                     />
                 </div>
