5e521738d9f1a5e43f4d7b3aebd6f42f9d37cc51
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-21 00:43:38 +0700

    Destructuring arguments

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-const ListGroup = (props) => {
-    const {items, textProperty, valueProperty, selectedItem, onItemSelect} = props;
+const ListGroup = ({items, textProperty, valueProperty, selectedItem, onItemSelect}) => {
 
     return (
         <ul className="list-group">
