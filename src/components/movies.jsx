211d92f63ed2fd8e266dbed6690f5028f2658669
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-20 07:21:13 +0700

    Adding all genres

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,8 @@ class Movies extends Component {
     };
 
     componentDidMount() {
-        this.setState({movies: getMovies(), genres: getGenres()});
+        const genres = [{name: "All Genres"}, ...getGenres()];
+        this.setState({movies: getMovies(), genres: genres});
     }
 
     handleDelete = (movie) => {
@@ -36,7 +37,7 @@ class Movies extends Component {
     };
 
     handleGenreSelect = (genre) => {
-        this.setState({selectedGenre: genre});
+        this.setState({selectedGenre: genre, currentPage: 1});
     };
 
     render() {
@@ -46,7 +47,10 @@ class Movies extends Component {
         if (count === 0)
             return <p>There are no movies in the database.</p>;
 
-        const filtered = selectedGenre ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
+        const filtered = selectedGenre && selectedGenre._id
+            ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+            : allMovies;
+
         const movies = paginate(filtered, currentPage, pageSize);
 
         return (
