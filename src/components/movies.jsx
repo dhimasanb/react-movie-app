46c27276ebb5e76bccacf6537c93cecab59728cd
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-20 18:23:37 +0700

    Raising the sort event

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,7 @@ class Movies extends Component {
     };
 
     componentDidMount() {
-        const genres = [{name: "All Genres"}, ...getGenres()];
+        const genres = [{_id: '', name: "All Genres"}, ...getGenres()];
         this.setState({movies: getMovies(), genres: genres});
     }
 
@@ -38,6 +38,10 @@ class Movies extends Component {
 
     handleGenreSelect = (genre) => {
         this.setState({selectedGenre: genre, currentPage: 1});
+    };
+
+    handleSort = (path) => {
+        console.log(path)
     };
 
     render() {
@@ -68,6 +72,7 @@ class Movies extends Component {
                         movies={movies}
                         onLike={this.handleLike}
                         onDelete={this.handleDelete}
+                        onSort={this.handleSort}
                     />
                     <Pagination
                         itemsCount={filtered.length}
