a7281644bbf0de57a81a92408f0cdcfc4a01934c
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-22 22:41:03 +0700

    Handling form submission

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
-import Login from "./components/login";
+import LoginForm from "./components/loginForm";
 import './App.css';
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
                 <NavBar/>
                 <main className="container">
                     <Switch>
-                        <Route path="/login" component={Login}></Route>
+                        <Route path="/login" component={LoginForm}></Route>
                         <Route path="/movies/:id" component={MovieForm}></Route>
                         <Route path="/movies" component={Movies}></Route>
                         <Route path="/customers" component={Customers}></Route>
