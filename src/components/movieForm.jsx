c3b4cecc6f927632b7b37d0598d549f787db127d
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-23 20:49:40 +0700

    Refactoring

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -24,20 +24,27 @@ class MovieForm extends Form {
         dailyRentalRate: Joi.number().required().min(0).max(10).label("Daily Rental Rate")
     };
 
-    async componentDidMount() {
+    async populateGenres() {
         const {data: genres} = await getGenres();
         this.setState({genres});
+    }
 
-        const movieId = this.props.match.params.id;
-        if (movieId === "new") return;
-
+    async populateMovie() {
         try {
+            const movieId = this.props.match.params.id;
+            if (movieId === "new") return;
+            
             const {data: movie} = await getMovie(movieId);
             this.setState({data: this.mapToViewModel(movie)});
         } catch (e) {
             if (e.response && e.response.status === 404)
                 this.props.history.replace("/not-found");
         }
+    }
+
+    async componentDidMount() {
+        await this.populateGenres();
+        await this.populateMovie();
     }
 
     mapToViewModel(movie) {
