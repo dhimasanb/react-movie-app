3b6c7f348e2bff7f1ee2a0452f2c85f729a3ff98
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-23 20:56:10 +0700

    Saving the movie

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -33,7 +33,7 @@ class MovieForm extends Form {
         try {
             const movieId = this.props.match.params.id;
             if (movieId === "new") return;
-            
+
             const {data: movie} = await getMovie(movieId);
             this.setState({data: this.mapToViewModel(movie)});
         } catch (e) {
@@ -57,8 +57,8 @@ class MovieForm extends Form {
         };
     }
 
-    doSubmit = () => {
-        saveMovie(this.state.data);
+    doSubmit = async () => {
+        await saveMovie(this.state.data);
 
         this.props.history.push("/movies");
     };
