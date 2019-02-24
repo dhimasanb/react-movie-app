0d0fde8341ef97ee28eacbd048e27c77de8eb2f9
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-24 17:40:18 +0700

    Getting the current user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Route, Redirect, Switch} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
+import jwtDecode from "jwt-decode";
 import Movies from "./components/movies";
 import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
@@ -13,11 +14,23 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
+    state = {};
+
+    componentDidMount() {
+        try {
+            const jwt = localStorage.getItem("token");
+            const user = jwtDecode(jwt);
+            this.setState({user});
+        } catch (e) {
+
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
                 <ToastContainer/>
-                <NavBar/>
+                <NavBar user={this.state.user}/>
                 <main className="container">
                     <Switch>
                         <Route path="/register" component={RegisterForm}/>
