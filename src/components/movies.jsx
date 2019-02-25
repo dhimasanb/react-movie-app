59852a8765f200999b7a660e77c7555582ed815c
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-02-25 10:01:07 +0700

    Showing or hiding elements based on the user

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -22,11 +22,11 @@ class Movies extends Component {
     };
 
     async componentDidMount() {
-        const { data } = await getGenres();
-        const genres = [{ _id: "", name: "All Genres" }, ...data];
+        const {data} = await getGenres();
+        const genres = [{_id: "", name: "All Genres"}, ...data];
 
-        const { data: movies } = await getMovies();
-        this.setState({ movies, genres });
+        const {data: movies} = await getMovies();
+        this.setState({movies, genres});
     }
 
     handleDelete = async (movie) => {
@@ -96,6 +96,7 @@ class Movies extends Component {
     render() {
         const {length: count} = this.state.movies;
         const {pageSize, currentPage, sortColumn, searchQuery} = this.state;
+        const {user} = this.props;
 
         if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -111,13 +112,15 @@ class Movies extends Component {
                     />
                 </div>
                 <div className="col">
-                    <Link
-                        to="/movies/new"
-                        className="btn btn-primary"
-                        style={{marginBottom: 20}}
-                    >
-                        New Movie
-                    </Link>
+                    {user && (
+                        <Link
+                            to="/movies/new"
+                            className="btn btn-primary"
+                            style={{marginBottom: 20}}
+                        >
+                            New Movie
+                        </Link>
+                    )}
                     <p>Showing {totalCount} movies in the database.</p>
                     <SearchBox value={searchQuery} onChange={this.handleSearch}/>
                     <MoviesTable
